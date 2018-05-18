d6f6135a1744c4a0a2b57ce890f30358e5f75d08
hieu2102 <gobanme.pierro@gmail.com>
2018-05-19 05:55:12 +0700

    add password hashing

--- loginAuthenticate.php
+++ loginAuthenticate.php
@@ -1,24 +1,32 @@
 <?php
 include './common/connect.php';
 session_start();
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
-    $query = mysqli_query($conn, "SELECT * FROM users WHERE 
-            password ='$password' AND 
-            email = '$email'");
-    $rows = mysqli_num_rows($query);
-    if ($rows == 0)
-    {
-        $_SESSION['message'] = alert_msg('danger', 'Invalid Email or Password');
-        header('location: login.php');
-    }else{
-        
+//query to db
+$query = mysqli_query($conn, "SELECT email, password FROM users WHERE email = '$email'");
+$rows = mysqli_num_rows($query);
+//check if email exist in db
+if ($rows == 0) {
+    $_SESSION['message'] = alert_msg('danger', 'Invalid Email');
+    header('location: login.php');
+} else {
+
+    //get result of $query as an object
+    $object = mysqli_fetch_object($query);
+    //get hashed password
+    $hashedP = $object->password;
+    //verify if input password == hashed password
+    if (password_verify($password, $hashedP)) {
         $_SESSION['login_user'] = $email;
         header('location: index.php');
-
+    }else{
+        $_SESSION['message'] = alert_msg('danger', 'Invalid Password');
+        header('location: login.php');
     }
-    mysqli_close($conn);
+}
+mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
--- registerAuthenticate.php
+++ registerAuthenticate.php
@@ -8,24 +8,26 @@ $re_password = $_POST['re_password'];
 $email = $_POST['email'];
 $id = $_POST['ID'];
 
-if ($password !== $re_password){
+if ($password !== $re_password) {
     $_SESSION['message'] = alert_msg('danger', 'Password and re-Password does not match');
     header('location: register.php');
 }
 
-$check = mysqli_query($conn, "SELECT * FROM users WHERE 
-            email = '$email' or 
+$check = mysqli_query($conn, "SELECT * FROM users WHERE
+            email = '$email' or
             id = '$id'
             ");
-if (mysqli_num_rows($check)>0){
+if (mysqli_num_rows($check) > 0) {
     $_SESSION['message'] = alert_msg('danger', 'Email or ID already registered');
     header('location: register.php');
 }
+//hash password using bcrypt
+$hashedP = password_hash($password, PASSWORD_BCRYPT);
 
-mysqli_query($conn, "INSERT INTO users 
+//store input data to database
+mysqli_query($conn, "INSERT INTO users
             (id, first_name, last_name, password, email)
-            VALUES ('$id', '$fname', '$lname', '$password', '$email')");
+            VALUES ('$id', '$fname', '$lname', '$hashedP', '$email')");
 
 $_SESSION['message'] = alert_msg('success', 'Account Created');
 header('location: login.php');
-?>
