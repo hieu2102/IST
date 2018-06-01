93933b3694c00ca12991c83ecb10cdb36a07f917
hieu2102 <gobanme.pierro@gmail.com>
2018-06-01 14:06:23 +0700

    postValidate escapeString && length check

--- page/postValidate.php
+++ page/postValidate.php
@@ -3,14 +3,16 @@ $content = $_POST['content'];
 $user = $_POST['userID'];
 $topic = $_POST['topicID'];
 
-if (strlen($content) <20){
+if (strlen(strip_tags($content)) < 20) {
     $_SESSION['message'] = alert_msg('danger', 'Post too short');
-    header ("location:".$_SERVER['HTTP_REFERER']);
-}else{ 
-//wrapping content
-$content = wordwrap($content, 100, "\n", true);
-$query = "INSERT INTO posts (content, post_by, post_topic) values ('$content', '$user', '$topic')";
-$sql = mysqli_query($conn, $query);
-$_SESSION['message'] = alert_msg('success', 'Post Submitted');
-header('location:index.php?page=topics&topicID='.$topic);
+    header("location:" . $_SERVER['HTTP_REFERER']);
+} else {
+
+    $content = mysqli_real_escape_string($conn, $content);
+    //wrapping content
+    $content = wordwrap($content, 100, "\n", true);
+    $query = "INSERT INTO posts (content, post_by, post_topic) values ('$content', '$user', '$topic')";
+    $sql = mysqli_query($conn, $query);
+    $_SESSION['message'] = alert_msg('success', 'Post Submitted');
+    header('location:index.php?page=topics&topicID=' . $topic);
 }
--- page/topicValidate.php
+++ page/topicValidate.php
@@ -7,26 +7,26 @@ $content = $_POST['content'];
 if (strlen($subject) < 20) {
     $_SESSION['message'] = alert_msg('danger', 'Subject length too short');
     header("location:" . $_SERVER['HTTP_REFERER']);
-} elseif (strlen($content) < 20) {
+} elseif (strlen(strip_tags($content)) < 20) {
     $_SESSION['message'] = alert_msg('danger', 'Post too short');
     header("location:" . $_SERVER['HTTP_REFERER']);
 } else {
+    $content = mysqli_real_escape_string($conn, $content);
     //wrapping content
     $content = wordwrap($content, 100, "\n", true);
-    
+
     //create new topic
     $topicQuery = "INSERT INTO topics (subject, topic_by, topic_cat) values ('$subject', '$userID', '$catID')";
     mysqli_query($conn, $topicQuery);
 
     //get topic ID
-    $topic = mysqli_query($conn, "SELECT max(id) from topics where topic_by = '$userID' and topic_cat = '$catID'");
+    $topic = mysqli_query($conn, "SELECT max(id) as cID from topics where topic_by = '$userID' and topic_cat = '$catID'");
     $topicID = mysqli_fetch_object($topic);
 
     //create first post
-    mysqli_query($conn, "INSERT INTO posts (content, post_by, post_topic) values ('$content', '$userID', '$topicID->id'");
+    mysqli_query($conn, "INSERT INTO posts (content, post_by, post_topic) values ('$content', '$userID', '$topicID->cID')");
 
-    $_SESSION['message'] = alert_msg('success', 'Topic Created');
-    header('location: index.php?page=category&ID='.$catID);
+    $_SESSION['message'] = alert_msg('success', 'Topic Created' . $topicID->cID);
+    header('location: index.php?page=category&ID={{$catID}}');
 
 }
-?>
