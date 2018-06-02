ffe342a5635984980b1a5518669cdacc75013338
hieu2102 <gobanme.pierro@gmail.com>
2018-06-02 20:08:50 +0700

    done paginating

--- admin/page/banlist.php
+++ admin/page/banlist.php
@@ -1,12 +1,26 @@
 <?php
+//pagination
+if (isset($_GET['pageno'])) {
+    $pageno = $_GET['pageno'];
+    if ($pageno <= 0) {
+        $pageno = 1;
+    }
+} else {
+    $pageno = 1;
+}
+$record_per_page = 10;
+$offset = ($pageno - 1) * $record_per_page;
+$total_records = mysqli_num_rows(mysqli_query($conn, "SELECT id from users where level = 'banned'"));
+$total_page = ceil($total_records / $record_per_page);
+
 //show all users
-$list = mysqli_query(
-    $conn, "SELECT
-    id, username, email, level
-    from users
-    where level = 'banned'
-    order by id asc"
-);
+$query = "SELECT
+id, username, email, level
+from users
+where level = 'banned'
+order by level desc
+limit ".$offset.",10";
+$list = mysqli_query($conn, $query);
 ?>
 
 <!DOCTYPE html>
@@ -47,5 +61,14 @@ $list = mysqli_query(
             ?>
         </tbody>
     </table>    
+    <nav>
+  <ul class="pagination">
+    <li><a class = 'page-link' href="index.php?page=banlist&pageno=<?=$pageno - 1?>">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="index.php?page=banlist&pageno=1">1</a></li>
+    <li class="page-item"><a class="page-link" href="index.php?page=banlist&pageno=2">2</a></li>
+    <li class="page-item"><a class="page-link" href="index.php?page=banlist&pageno=3">3</a></li>
+    <li class="page-item"><a class="page-link" href="index.php?page=banlist&pageno=<?=$pageno + 1?>">Next</a></li>
+  </ul>
+</nav>
 </body>
 </html>
--- admin/page/usersManage.php
+++ admin/page/usersManage.php
@@ -1,12 +1,27 @@
 <?php
+
+//pagination
+if (isset($_GET['pageno'])) {
+    $pageno = $_GET['pageno'];
+    if ($pageno <= 0) {
+        $pageno = 1;
+    }
+} else {
+    $pageno = 1;
+}
+$record_per_page = 10;
+$offset = ($pageno - 1) * $record_per_page;
+$total_records = mysqli_num_rows(mysqli_query($conn, "SELECT id from users where level != 'banned'"));
+$total_page = ceil($total_records / $record_per_page);
+
 //show all users
-$list = mysqli_query(
-    $conn, "SELECT
-    id, username, email, level
-    from users
-    where level != 'banned'
-    order by level desc"
-);
+$query = "SELECT
+id, username, email, level
+from users
+where level != 'banned'
+order by level desc
+limit ".$offset.",10";
+$list = mysqli_query($conn, $query);
 ?>
 
 <!DOCTYPE html>
@@ -33,39 +48,48 @@ $list = mysqli_query(
         </thead>
         <tbody>
             <?php
-            while ($row = mysqli_fetch_object($list)){ ?>
+while ($row = mysqli_fetch_object($list)) {?>
                     <tr>
                         <td><?=$row->id?></td>
                         <td><?=$row->username?></td>
                         <td><?=$row->email?></td>
                         <td><?=$row->level?></td>
                         <td>
-                        <?php if ($row->level !='banned'){ ?>
+                        <?php if ($row->level != 'banned') {?>
                         <form method = "POST" action="index.php?function=user-set-level&userID=<?=$row->id?>">
                             <input type="hidden" name = 'level' value = "banned">
                             <input type="submit" class = 'btn btn-danger' name = 'submit' value = "Ban" >
                         </form>
-                        <?php } ?>
-                        <td> 
-                        <?php if ($row->level =='normal'){ ?>
+                        <?php }?>
+                        <td>
+                        <?php if ($row->level == 'normal') {?>
                             <form method = "POST" action="index.php?function=user-set-level&userID=<?=$row->id?>">
                             <input type="hidden" name = 'level' value = "admin">
                             <input type="submit" class = 'btn btn-primary' name = 'submit' value = "Set Admin" >
                             </form>
-                        <?php } ?>
+                        <?php }?>
 
-                        <?php if ($row->level == 'admin'){ ?>
+                        <?php if ($row->level == 'admin') {?>
                             <form method = "POST" action="index.php?function=user-set-level&userID=<?=$row->id?>">
                             <input type="hidden" name = 'level' value = "normal">
                             <input type="submit" class = 'btn btn-info' name = 'submit' value = "Unset Admin" >
                             </form>
-                        <?php } ?>
+                        <?php }?>
                         </td>
                     </tr>
-                
+
            <?php }
-            ?>
+?>
         </tbody>
-    </table>    
+    </table>
+    <nav>
+  <ul class="pagination">
+    <li><a class = 'page-link' href="index.php?page=usersManage&pageno=<?=$pageno - 1?>">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="index.php?page=usersManage&pageno=1">1</a></li>
+    <li class="page-item"><a class="page-link" href="index.php?page=usersManage&pageno=2">2</a></li>
+    <li class="page-item"><a class="page-link" href="index.php?page=usersManage&pageno=3">3</a></li>
+    <li class="page-item"><a class="page-link" href="index.php?page=usersManage&pageno=<?=$pageno + 1?>">Next</a></li>
+  </ul>
+</nav>
 </body>
 </html>
