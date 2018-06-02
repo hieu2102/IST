54d641ebc5a0017b154edb5f1d76cf64e2767452
hieu2102 <gobanme.pierro@gmail.com>
2018-06-02 23:09:43 +0700

    register validation

--- auth/registerAuthenticate.php
+++ auth/registerAuthenticate.php
@@ -7,43 +7,53 @@ $password = $_POST['password'];
 $re_password = $_POST['re_password'];
 $email = $_POST['email'];
 $id = $_POST['ID'];
-
+$namecheck = str_replace(" ", "", $name);
+$idcheck = substr($id, 0, 2);
 //check passwords input
-if ($password !== $re_password) { //fail passwords input check
-    $_SESSION['message'] = alert_msg('danger', 'Password and re-Password does not match');
+if (strlen($password) < 8) {
+    $_SESSION['message'] = alert_msg('danger', 'Password must be longer than 8 characters');
     header('location: register.php');
+
 } else {
-    if (strlen($name)<6 ||strlen($name>30)){
-        $_SESSION['message'] = alert_msg('danger', 'Username must be 6-30 characters long');
-    }
-    //check for existing email or id
-    $check = mysqli_query($conn, "SELECT * FROM users WHERE
-            email = '$email' or
-            id = '$id'
-            ");
-    $rows = mysqli_num_rows($check);
-    if ($rows >= 1) { //fail email or id check
-        $_SESSION['message'] = alert_msg('danger', 'Email or ID already registered');
+    if ($password !== $re_password) { //fail passwords input check
+        $_SESSION['message'] = alert_msg('danger', 'Password and re-Password does not match');
         header('location: register.php');
     } else {
-        //check name contains illegal characters
-        //fail name check
-        if (!(ctype_alnum($name))) {
-            $_SESSION['message'] = alert_msg('danger', 'Name contains special characters \n' . $name);
+        //name length check
+        if (strlen($name) < 6 || strlen($name > 30)) {
+            $_SESSION['message'] = alert_msg('danger', 'Invalid Username');
             header('location: register.php');
-        }
-        //passed all check, proceed to account creation
-        else {
-            //hash password using bcrypt
-            $hashedP = password_hash($password, PASSWORD_BCRYPT);
+        } else {
+            //check name contains illegal characters
+            if (!(ctype_alnum($namecheck))) {
+                $_SESSION['message'] = alert_msg('danger', 'Name contains special characters ' . $name);
+                header('location: register.php');
+            } else {
+                //id check
+                if ($idcheck > 18 || strlen($id) != 10) {
+                    $_SESSION['message'] = alert_msg('danger', 'Invalid ID');
+                    header('location: register.php');
 
-            //store input data to database
-            mysqli_query($conn, "INSERT INTO users
-            (id, username, password, email)
-            VALUES ('$id', '$name', '$hashedP', '$email')");
+                } else {
+                    //check for existing email or id
+                    $check = mysqli_query($conn, "SELECT * FROM users WHERE email = '$email' or id = '$id' ");
+                    $rows = mysqli_num_rows($check);
+                    if ($rows >= 1) { //fail email or id check
+                        $_SESSION['message'] = alert_msg('danger', 'Email or ID already registered');
+                        header('location: register.php');
+                    }
+                    //passed all check, proceed to account creation
+                    else {
+                        //hash password using bcrypt
+                        $hashedP = password_hash($password, PASSWORD_BCRYPT);
 
-            $_SESSION['message'] = alert_msg('success', 'Account Created ');
-            header('location: login.php');
+                        //store input data to database
+                        mysqli_query($conn, "INSERT INTO users (id, username, password, email) VALUES ('$id', '$name', '$hashedP', '$email')");
+                        $_SESSION['message'] = alert_msg('success', 'Account Created ');
+                        header('location: login.php');
+                    }
+                }
+            }
         }
     }
 }
